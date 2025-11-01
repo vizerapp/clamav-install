0f0de0e8fc14c78a50e0bc87d7181e90a940a762
Kiah Stroud <kiah@kiah.dev>
2025-10-31 17:08:15 -0700

    revert commits related to supporting non-ARM CPUs
    This reverts commits:
    - b205a7b7f7eb061c6767d23c0ae4d4c8b7edc1d9.
    - 50d435e389bf16b4f66bf7b803a7e5149cf03d53
    - 3d6ae9a267e80133d97fc0b2a6ee45194233e2e5
    - a81a50723e2a3e189220a379f502d781e12e0361

--- install.sh
+++ install.sh
@@ -10,15 +10,6 @@ if ! command -v curl &>/dev/null; then
     exit 1
 fi
 
-ARCHITECTURE=$(arch)
-BASE_PATH=/opt/homebrew/etc
-LOG_PATH=/opt/homebrew/var/log
-
-if [[ "$ARCHITECTURE" == "i386" ]]; then
-    BASE_PATH=/usr/local/etc
-    LOG_PATH=/var/log
-fi
-
 echo "Running clamav setup"
 echo "🚨 IMPORTANT: you will soon be prompted for your machine's password, please watch this script run."
 echo "Note: your password will not be visible while you type."
@@ -28,45 +19,27 @@ brew install clamav
 
 echo "Downloading configs..."
 sudo curl -s https://raw.githubusercontent.com/vizerapp/clamav-install/HEAD/assets/clamd.conf \
-    -o "$BASE_PATH/clamav/clamd.conf"
+    -o /opt/homebrew/etc/clamav/clamd.conf
 sudo curl -s https://raw.githubusercontent.com/vizerapp/clamav-install/HEAD/assets/freshclam.conf \
-    -o "$BASE_PATH/clamav/freshclam.conf"
-if [[ "$ARCHITECTURE" == "i386" ]]; then
-    sudo sed -i '' "s|/opt/homebrew/etc|/usr/local|g" "$BASE_PATH/clamav/freshclam.conf"
-    sudo sed -i '' "s|/opt/homebrew/etc|/usr/local|g" "$BASE_PATH/clamav/clamd.conf"
-fi
+    -o /opt/homebrew/etc/clamav/freshclam.conf
 
 echo "Setting up clamav..."
-sudo mkdir "$BASE_PATH/clamav/bin"
-sudo mkdir "$BASE_PATH/clamav/quarantine"
+sudo mkdir /opt/homebrew/etc/clamav/{bin,quarantine}
 sudo curl -s https://raw.githubusercontent.com/vizerapp/clamav-install/HEAD/assets/bin/notify \
-    -o "$BASE_PATH/clamav/bin/notify"
+    -o /opt/homebrew/etc/clamav/bin/notify
 sudo curl -s https://raw.githubusercontent.com/vizerapp/clamav-install/HEAD/assets/bin/scan_downloads \
-    -o "$BASE_PATH/clamav/bin/scan_downloads"
+    -o /opt/homebrew/etc/clamav/bin/scan_downloads
 sudo curl -s https://raw.githubusercontent.com/vizerapp/clamav-install/HEAD/assets/bin/scan_home \
-    -o "$BASE_PATH/clamav/bin/scan_home"
-sudo chown -R clamav:clamav "$BASE_PATH/clamav/bin"
-sudo chown -R clamav:clamav "$BASE_PATH/clamav/quarantine"
-sudo chown -R clamav:clamav "$BASE_PATH/clamav/freshclam.conf"
-sudo chown -R clamav:clamav "$BASE_PATH/clamav/clamd.conf"
-sudo chmod -R 770 "$BASE_PATH/clamav/bin"
-sudo chmod -R 640 "$BASE_PATH/clamav/quarantine"
-sudo chmod -R 640 "$BASE_PATH/clamav/freshclam.conf"
-sudo chmod -R 640 "$BASE_PATH/clamav/clamd.conf"
-if [[ "$ARCHITECTURE" == "i386" ]]; then
-    sudo sed -i '' "s|/opt/homebrew/etc/clamav|/usr/local/etc/clamav|g" "$BASE_PATH/clamav/bin/notify"
-    sudo sed -i '' "s|/opt/homebrew/etc/clamav|/usr/local/etc/clamav|g" "$BASE_PATH/clamav/bin/scan_downloads"
-    sudo sed -i '' "s|/opt/homebrew/etc/clamav|/usr/local/etc/clamav|g" "$BASE_PATH/clamav/bin/scan_home"
-fi
+    -o /opt/homebrew/etc/clamav/bin/scan_home
+sudo chown -R clamav:clamav /opt/homebrew/etc/clamav/{bin,quarantine,freshclam.conf,clamd.conf}
+sudo chmod -R 770 /opt/homebrew/etc/clamav/bin
+sudo chmod -R 640 /opt/homebrew/etc/clamav/{quarantine,freshclam.conf,clamd.conf}
 
 echo "Setting up logs"
-sudo mkdir -p "$LOG_PATH/"
-sudo touch "$LOG_PATH/freshclam.log"
-sudo touch "$LOG_PATH/clamdscan.log"
-sudo chown clamav:clamav "$LOG_PATH/freshclam.log"
-sudo chown clamav:clamav "$LOG_PATH/clamdscan.log"
-sudo chmod 644 "$LOG_PATH/freshclam.log"
-sudo chmod 644 "$LOG_PATH/clamdscan.log"
+sudo mkdir -p /opt/homebrew/var/log/
+sudo touch /opt/homebrew/var/log/{freshclam,clamdscan}.log
+sudo chown clamav:clamav /opt/homebrew/var/log/{freshclam,clamdscan}.log
+sudo chmod 644 /opt/homebrew/var/log/{freshclam,clamdscan}.log
 
 echo "Starting clamd..."
 sudo brew services start clamav
@@ -78,17 +51,7 @@ sudo curl -s https://raw.githubusercontent.com/vizerapp/clamav-install/HEAD/asse
     -o /Library/LaunchDaemons/com.vizerapp.clamav.clamdscan.home.plist
 sudo curl -s https://raw.githubusercontent.com/vizerapp/clamav-install/HEAD/assets/daemons/com.vizerapp.clamav.freshclam.plist \
     -o /Library/LaunchDaemons/com.vizerapp.clamav.freshclam.plist
-sudo chmod 644 /Library/LaunchDaemons/com.vizerapp.clamav.freshclam.plist
-sudo chmod 644 /Library/LaunchDaemons/com.vizerapp.clamav.clamdscan.downloads.plist
-sudo chmod 644 /Library/LaunchDaemons/com.vizerapp.clamav.clamdscan.home.plist
-if [[ "$ARCHITECTURE" == "i386" ]]; then
-    sudo sed -i '' "s|/opt/homebrew/etc/clamav|/usr/local/etc/clamav|g" /Library/LaunchDaemons/com.vizerapp.clamav.freshclam.plist
-    sudo sed -i '' "s|/opt/homebrew/etc/clamav|/usr/local/etc/clamav|g" /Library/LaunchDaemons/com.vizerapp.clamav.clamdscan.downloads.plist
-    sudo sed -i '' "s|/opt/homebrew/etc/clamav|/usr/local/etc/clamav|g" /Library/LaunchDaemons/com.vizerapp.clamav.clamdscan.home.plist
-    sudo sed -i '' "s|/opt/homebrew/var/log|/var/log|g" /Library/LaunchDaemons/com.vizerapp.clamav.freshclam.plist
-    sudo sed -i '' "s|/opt/homebrew/var/log|/var/log|g" /Library/LaunchDaemons/com.vizerapp.clamav.clamdscan.downloads.plist
-    sudo sed -i '' "s|/opt/homebrew/var/log|/var/log|g" /Library/LaunchDaemons/com.vizerapp.clamav.clamdscan.home.plist
-fi
+sudo chmod 644 /Library/LaunchDaemons/com.vizerapp.clamav.{clamdscan.downloads,clamdscan.home,freshclam}.plist
 
 echo "Starting daemons"
 sudo launchctl load /Library/LaunchDaemons/com.vizerapp.clamav.freshclam.plist
