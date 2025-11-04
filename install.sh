1aa987c501a0b344bc4cfc16d0762a4ae0b96a91
Kiah Stroud <kiah@kiah.dev>
2025-11-04 15:46:23 -0800

    add daemon to scan volumes

--- install.sh
+++ install.sh
@@ -31,6 +31,8 @@ sudo curl -s https://raw.githubusercontent.com/vizerapp/clamav-install/HEAD/asse
     -o /opt/homebrew/etc/clamav/bin/scan_downloads
 sudo curl -s https://raw.githubusercontent.com/vizerapp/clamav-install/HEAD/assets/bin/scan_home \
     -o /opt/homebrew/etc/clamav/bin/scan_home
+sudo curl -s https://raw.githubusercontent.com/vizerapp/clamav-install/HEAD/assets/bin/scan_volumes \
+    -o /opt/homebrew/etc/clamav/bin/scan_volumes
 sudo chown -R clamav:clamav /opt/homebrew/etc/clamav/{bin,quarantine,freshclam.conf,clamd.conf}
 sudo chmod -R 770 /opt/homebrew/etc/clamav/bin
 sudo chmod -R 640 /opt/homebrew/etc/clamav/{quarantine,freshclam.conf,clamd.conf}
@@ -49,29 +51,41 @@ sudo curl -s https://raw.githubusercontent.com/vizerapp/clamav-install/HEAD/asse
     -o /Library/LaunchDaemons/com.vizerapp.clamav.clamdscan.downloads.plist
 sudo curl -s https://raw.githubusercontent.com/vizerapp/clamav-install/HEAD/assets/daemons/com.vizerapp.clamav.clamdscan.home.plist \
     -o /Library/LaunchDaemons/com.vizerapp.clamav.clamdscan.home.plist
+sudo curl -s https://raw.githubusercontent.com/vizerapp/clamav-install/HEAD/assets/daemons/com.vizerapp.clamav.clamdscan.volumes.plist \
+    -o /Library/LaunchDaemons/com.vizerapp.clamav.clamdscan.volumes.plist
 sudo curl -s https://raw.githubusercontent.com/vizerapp/clamav-install/HEAD/assets/daemons/com.vizerapp.clamav.freshclam.plist \
     -o /Library/LaunchDaemons/com.vizerapp.clamav.freshclam.plist
-sudo chmod 644 /Library/LaunchDaemons/com.vizerapp.clamav.{clamdscan.downloads,clamdscan.home,freshclam}.plist
+sudo chmod 644 /Library/LaunchDaemons/com.vizerapp.clamav.freshclam.plist
+sudo chmod 644 /Library/LaunchDaemons/com.vizerapp.clamav.clamdscan.downloads.plist
+sudo chmod 644 /Library/LaunchDaemons/com.vizerapp.clamav.clamdscan.home.plist
+sudo chmod 644 /Library/LaunchDaemons/com.vizerapp.clamav.clamdscan.volumes.plist
 
 echo "Starting daemons"
 sudo launchctl load /Library/LaunchDaemons/com.vizerapp.clamav.freshclam.plist
 sleep 5 # give freshclam a few moments to run
 sudo launchctl load /Library/LaunchDaemons/com.vizerapp.clamav.clamdscan.downloads.plist
 sudo launchctl load /Library/LaunchDaemons/com.vizerapp.clamav.clamdscan.home.plist
+sudo launchctl load /Library/LaunchDaemons/com.vizerapp.clamav.clamdscan.volumes.plist
 
 echo "Downloading test virus files..."
 curl -s https://amtso.eicar.org/eicar.com -o ~/Downloads/eicar-standard-antivirus-test-file-d
 cp ~/Downloads/eicar-standard-antivirus-test-file-d ~/eicar-standard-antivirus-test-file-h
+sudo mkdir /Volumes/clamav-virus-test
+sudo cp ~/Downloads/eicar-standard-antivirus-test-file-d /Volumes/clamav-virus-test/eicar-standard-antivirus-test-file-v
 
-echo 'Within 15 seconds, the eicar test files should be quarantined by clamav'
+echo 'Within 60 seconds, two of the three eicar test files should be quarantined by clamav'
 echo '🚨 IMPORTANT: please accept all permission requests from "clamd" and "clamdscan"'
 
-i=15
+i=60
 while [ $i -gt 0 ]; do
     printf "\r$i"
     ((i--))
     sleep 1
 done
 echo
+
+echo "Running cleanup"
+sudo rm -rfv /Volumes/clamav-virus-test
+sudo rm -vf /opt/homebrew/etc/clamav/quarantine/eicar-standard-antivirus-test-file-*
 
 echo "✅ clamav setup complete"
